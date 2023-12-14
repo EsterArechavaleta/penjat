92938c49fa4582143ed3d999884b983d88179a8e
Ester <parechavaleta19671@iesjoanramis.org>
2023-12-14 17:05:27 +0100

    final result

--- penjat.js
+++ penjat.js
@@ -1,82 +1,253 @@
 // Captuar elementos del DOM
 const contenidor = document.querySelector('.contenidor');
-const letra = document.querySelectorAll('.fila .letra:not(.fallada)');
-const contador = document.getElementById('contador');
-const palabraSelect = "";
-const palabraSecreta = ['perro', 'niño', 'tortuga', 'coche'];
+const categoria = document.querySelector('.categoria');
+const letra = document.querySelectorAll('.letra'); // cada letra del teclado para elegir
+const contadorIntentos = document.getElementById('contadorIntentos'); // contador de intentos restantes
+//const palabraSecreta = ['PERRO', 'NENE', 'TORTUGA', 'COCHE']; // array de palabras secretas para elegir
+const inicio = document.getElementById('inicio'); // boton para reiniciar el juego
+const palabra = document.getElementById('palabra');// para mostrar la palabra en pantalla y cambiar por guiones
+const contadorErrores = document.getElementById('contadorErrores'); // contador de las letras erróneas
+const segundos = document.getElementById('segundos'); // el hueco del reloj para los segundos
+const minutos = document.getElementById('minutos'); // hueco para los minutos
+const horas = document.getElementById('horas'); // hueco para las horas
+segundos.innerText = '00'; // valor inicial de segundos
+minutos.innerText = '01'; // valor inicial de minutos
+horas.innerText = '00'; // valor inicial de horas
+let letrasAcertadas = [];
+let arrayIntento = [];
 
+const envoltorio_victoria = document.getElementById('envoltorio-victoria');
+const envoltorio_derrota = document.getElementById('envoltorio-derrota');
+const cerrar = document.getElementById('cerrar-popup');
+//elementos para la cuenta atras
+let s;
+let m;
+let h;
+let fecha = new Date();
+let cuentaAtra;
+// Crear variable para guardar palabra seleccionada
+let  palabraSelect = "";
+// palabras para temático
+const categoriaAnimales = ['PERRO', 'GATO', 'TORTUGA', 'ELEFANTE', 'PERIQUITO'];
+const categoriaMediosTransporte = ['COCHE', 'AVION', 'BARCO', 'SUBMARINO', 'MOTO'];
+const categoriaCapitales = ['PARIS', 'LONDRES', 'MADRID', 'WASHINGTON', 'LISBOA'];
+const categoriaAlimentos = ['CHOCOLATE', 'CHORIZO', 'GALLETA', 'AZUCAR', 'HELADO'];
 
-// Precio de la pelicula seleccionada.
-//let preuDelTicket = +peliculaSelect.value;
-
-// Llenamos la interfície de usuario.
-ompleUI();
 
 /* FUNCIONES */
-function eligePalabra(){
-  palabraSelect = [Math.floor(Math.random()*palabraSecreta.length)]
 
-  foreach 
+//función para elegir la palabra secreta del array y pasar las letras a mayúsculas para comparar
+function eligePalabra(array) {
+  palabraSelect = array[Math.floor(Math.random()*array.length)];
+  palabraSelect = palabraSelect.toUpperCase();
+  console.log(palabraSelect);
+  ponGuion(palabraSelect);
+ console.log(palabra);
 }
 
-function actualizaSeleccionLetra() {
-  const letrasFalladas =
-        document.querySelectorAll('.fila .letra.fallada');
-        
-        // const seientsIndex =
-        //   [...seientsSeleccionats].map(function(seient) {
-        //     return [...seients].indexOf(seient);
-        //   });
-
-  const letrasIndex =
-      [...letrasFalladas].map((letra) => [...letras].indexOf(letra));
-
-  localStorage.setItem('letrasFalladas', JSON.stringify(letrasIndex));
-
-  const contadorLetrasFalladas = letrasFalladas.length;
-  contador.innerText = contadorLetrasFalladas;
-
+//función para sustituir las letras de la palabra por guiones en pantalla
+function ponGuion (palabraSelect) {
+  arrayIntento = new Array(palabraSelect.length);
+  arrayIntento.fill('_');
+  palabra.innerHTML = " ";
+  for (let i = 0; i<arrayIntento.length; i++) {
+    
+    palabra.innerHTML += " " + arrayIntento[i];
+  };
 }
 
-function ompleUI() {
-  const letrasFalladas = JSON.parse(localStorage.getItem('letrasFalladas'));
-  
-  if(letrasFalladas !== null && letrasFalladas.length > 0) {
-    letras.forEach((letra, index) => {
-      if(LetraFallada.indexOf(index) > -1) {
-        letra.classList.add('fallada');
+//función para cambiarles la clase fallada o acertada para cambiarles el color
+function intento(letra) {
+  let contador =0;
+    for (let i = 0; i < palabraSelect.length; i++){
+      if (palabraSelect[i] == letra.innerHTML){
+        letrasAcertadas.push(palabraSelect[i]);
+        palabra[i] = letra;
+        contador++;
       }
-    });
+    }
+    console.log(letrasAcertadas);
+    if (contador > 0){
+    letra.classList.add('acertada');
+    actualizarPalabra(letra)
+  } else {
+    contadorErrores.innerHTML = parseInt(contadorErrores.innerHTML) + 1;
+    contadorIntentos.innerHTML = contadorIntentos.innerHTML - 1;
+    letra.classList.add('fallada');
+  }
+  mostrarResultado();
+
+}
+
+//funcion para colocar las letras acertadas en su sitio
+function actualizarPalabra(letra){
+  for(let i = 0; i < arrayIntento.length; i++){
+    if(letra.classList. contains('acertada')){
+      if(letra.innerHTML == palabraSelect[i]){
+        arrayIntento[i] = letra.innerHTML;
+      }
+    }
+  }
+  palabra.innerHTML = null;
+  for ( let i = 0; i < arrayIntento.length; i++){
+    palabra.innerHTML += " " + arrayIntento[i];
+  }
+}
+
+//funcion para volver las letras al inicio quitando color
+function borraColor() {
+  letra.forEach ( (lletra, indice) => {
+    if( lletra.classList.contains('acertada') || lletra.classList.contains('fallada')){
+      lletra.classList.value = 'letra';
+    }
+  })
+}
+
+//funciones para la cuenta atrás de los 
+function cuentaAtras() {
+  if (m == 0 && h ==0 && s == 0) {
+    segundos.innerText = '00';
+    minutos.innerText = '00';
+    horas.innerText = '00';
+  } else {
+    s = s % 60;
+    if (s == 0){
+      m--;
+      s = 60;
+    }
+    s--;
   }
 
+  if (s < 10) { 
+    s = '0' + s;
+  }
+
+  if (m < 10) {
+    m = '0' + m;
+  }
   
+  fecha.setSeconds(s);
+  segundos.innerText = s;
+  minutos.innerText = m;
+  if (m.length > 2) {
+    minutos.innerText = '00';
+  }
+  if (s.length > 2){
+    segundos.innerText = '00';
+  }
+}
 
+function empezarCuentaAtras() {
+  fecha.setHours(0, 1, 0, 0);
+  h = fecha.getHours();
+  m = fecha.getMinutes();
+  s = fecha.getSeconds();
+  segundos.innerText = '00';
+  minutos.innerText = '01';
+  horas.innerText = '00';
+  cuentaAtra = setInterval(cuentaAtras, 1000);
+
+}
+
+//funcion para mostrar los pop up de victoria o derrota y parar la cuenta atrás
+function mostrarResultado() {
+  if(letrasAcertadas.length == palabraSelect.length || contadorIntentos.innerHTML == '0' || (m == "00" && s == "00")){
+
+    clearInterval(cuentaAtra);
+
+    if (letrasAcertadas.length == palabraSelect.length) {
+    
+      //mostrar pop up victoria
+      envoltorio_victoria.style.display = 'block';
+      // Click sobre el boton para cerrar el pop-up
+      cerrar.addEventListener('click', () => {
+      envoltorio_victoria.style.display = 'none';
+      location.reload();
+      });
+      //Click sobre el envoltorio -> cerrar el pop-up
+      envoltorio_victoria.addEventListener('click', () => {
+      envoltorio_victoria.style.display = 'none';
+      location.reload();
+      });
+    }
+
+    if (contadorIntentos.innerHTML == '0' ) {
+    
+      //mostrar pop up derrota
+      envoltorio_derrota.style.display = 'block';
+      // Click sobre el boton para cerrar el pop-up
+      cerrar.addEventListener('click', () => {
+      envoltorio_derrota.style.display = 'none';
+      location.reload();
+      });
+      //Click sobre el envoltorio -> cerrar el pop-up
+      envoltorio_derrota.addEventListener('click', () => {
+      envoltorio_derrota.style.display = 'none';
+      location.reload();
+      });
+    } 
   
-
-  const preuPeliculaSeleccionada = localStorage.getItem('preuPeliculaSeleccionada');
-
-  if(preuPeliculaSeleccionada !== null) {
-    preuDelTicket = +preuPeliculaSeleccionada;
+    if (m == "00" && s == "00") {
+      //mostrar pop up derrota
+      envoltorio_derrota.style.display = 'block';
+      // Click sobre el boton para cerrar el pop-up
+      cerrar.addEventListener('click', () => {
+      envoltorio_derrota.style.display = 'none';
+      location.reload();
+      });
+      //Click sobre el envoltorio -> cerrar el pop-up
+      envoltorio_derrota.addEventListener('click', () => {
+      envoltorio_derrota.style.display = 'none';
+      location.reload();
+      });
+    }
   }
 }
 
 /* EVENTOS */
 
 contenidor.addEventListener('click', (e) => {
+  console.log(e.target.classList.value);
   if(e.target.classList.contains('letra')
-      && !e.target.classList.contains('fallada')) {
-    e.target.classList.toggle('seleccionat');
-    actualitzaSeleccioSeients();
+      && e.target.classList.contains('fallada')) {
+    
+    actualizaLetraFallada();
+  }else if(e.target.classList.contains('letra')){
+    console.log(e.target.innerHTML);
+    intento(e.target);
   }
 });
 
-peliculaSelect.addEventListener('change', (e) => {
-  preuDelTicket = +e.target.value;
+categoria.addEventListener('click', (e) => {
+  if(e.target.value == 'animales') {
+    eligePalabra(categoriaAnimales);
+  }
 
-  localStorage.setItem('indexPeliculaSeleccionada', e.target.selectedIndex);
-  localStorage.setItem('preuPeliculaSeleccionada', e.target.value);
+  if(e.target.value == 'mtransporte') {
+    eligePalabra(categoriaMediosTransporte);
+  }
 
-  actualitzaSeleccioSeients();
+  if(e.target.value == 'capitales') {
+    eligePalabra(categoriaCapitales);
+  } 
+
+  if(e.target.value == 'alimentos') {
+    eligePalabra(categoriaAlimentos);
+  }
 });
 
-actualitzaSeleccioSeients();
+inicio.addEventListener('click', iniciaJuego);
+
+function iniciaJuego() {
+  clearInterval(cuentaAtra);
+  palabra.innerHTML =" ";
+  ponGuion(palabraSelect);
+  empezarCuentaAtras();
+  contadorIntentos.innerHTML = 7;
+  contadorErrores.innerHTML = 0;
+  borraColor();
+  categoria.options[0].selected = 'selected';
+  eligePalabra();
+  //mostrarResultado();
+  //clearInterval();
+};
